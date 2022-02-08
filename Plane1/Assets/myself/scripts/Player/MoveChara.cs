7a670ebb44f780f0e59dc5e7a4447297acd7bda4
HotatePotato <smjs2469@gmail.com>
2022-02-08 10:50:12 +0900

    操作をキーボード側からもできるようにした
    Pボタンを押すことでキーボード&マウス、コントローラ操作を切り替え可能
    きーぼど操作の仕方
    左旋回：A　右旋回：D　上昇下降：WS
    エイム：マウスドラッグ
    射撃　左クリック

--- Plane1/Assets/myself/scripts/Player/MoveChara.cs
+++ Plane1/Assets/myself/scripts/Player/MoveChara.cs
@@ -7,6 +7,10 @@ public class MoveChara : MonoBehaviour
     public float speed = 10.0f;
         public Rigidbody rb;
 
+        private bool keyboard=false;//操作方法についてキーボードかコントローラか
+        //RotateSkyV2 にも同じスクリプトがある
+        //MoveChara,moveAimにも同じスクリプトがある
+
         void Start() 
         {
             rb = GameObject.Find("Main (1)").GetComponent<Rigidbody>();
@@ -18,11 +22,8 @@ public class MoveChara : MonoBehaviour
             //float x =  Input.GetAxis("Horizontal") * speed;
             //float z = Input.GetAxis("Vertical") * speed;
             //rb.AddForce(transform.forward*20,ForceMode.Force);
-            if (Input.GetKey ("joystick button 0")) {
-            transform.position += transform.forward*2;
-            }
             float lsv = Input.GetAxis ("L_Stick_V");//左スティックの傾き　タテ
-            if(lsv!=0)
+            /*if(lsv!=0)
             {
                /* Transform myTransform = GameObject.Find("Main (1)").transform;
 
@@ -35,10 +36,31 @@ public class MoveChara : MonoBehaviour
                 */
                 Transform aatransform=GameObject.Find("Player1").transform;
                 transform.position+=2*aatransform.forward;
+            //}
+
+            //else 
+            transform.position += transform.forward;
+            if(keyboard==false)
+            {
+                if (Input.GetKey ("joystick button 0")) 
+                {
+                transform.position += transform.forward*2;
+                }
             }
             else 
             {
-                transform.position += transform.forward;
+                if (Input.GetKey(KeyCode.Space)) 
+                {
+                    Debug.Log("aaa");
+                transform.position += transform.forward*2;
+                }
+            }
+            if(Input.GetKey(KeyCode.P))//きーぼど操作ORコントローラ操作
+            //shootingにも同じスクリプトあるからそこも書き換えること
+            //MoveChara,moveAimにも同じスクリプトがある
+            {
+                if(keyboard==false)keyboard=true;
+                else keyboard=false;
             }
         }
 }
